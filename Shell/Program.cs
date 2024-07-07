13ed64b560b1c72b1f3d2e2576bb2e30a47207de
Lxdovic <ludovicdebever0@gmail.com>
2024-07-07 16:26:31 +0200

    init: basic program with exit command

---
+++ Shell/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Shell;
+
+internal class Program {
+    private static void Main() {
+        Run();
+    }
+
+    private static void Run() {
+        Console.Write("$ ");
+        var command = Console.ReadLine()?.Split(" ");
+
+        switch (command?[0]) {
+            case "exit":
+                HandleExit(command);
+                break;
+            default:
+                CommandNotFound(command);
+                break;
+        }
+
+        Run();
+    }
+
+    private static void HandleExit(string[] command) {
+        var exitCode = 0;
+
+        if (command.Length > 1) exitCode = int.Parse(command[1]);
+
+        Environment.Exit(exitCode);
+    }
+
+    private static void CommandNotFound(string[]? command) {
+        Console.WriteLine($"{command?[0]} command not found.");
+    }
+}
