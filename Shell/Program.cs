67a14fc5557b4d95dc334be418acfa5e1a90ecb5
Lxdovic <ludovicdebever0@gmail.com>
2024-07-09 16:53:01 +0200

    feat!: started implementing running local binaries

--- Shell/Program.cs
+++ Shell/Program.cs
@@ -1,4 +1,5 @@
-﻿using Shell.Commands;
+﻿using System.Diagnostics;
+using Shell.Commands;
 
 namespace Shell;
 
@@ -8,7 +9,7 @@ internal class Program {
         { "echo", new EchoCommand() },
         { "type", new TypeCommand() }
     };
-
+    
     private static void Main() {
         Run();
     }
@@ -22,14 +23,49 @@ internal class Program {
             if (command == null || command.Length == 0) continue;
 
             BuiltinCommands.TryGetValue(command[0], out var commandToExecute);
-
-            if (commandToExecute == null) {
-                CommandNotFound(command);
+            
+            if (commandToExecute != null) {
+                commandToExecute.Run(command.Skip(1).ToArray());
                 continue;
             }
-
-            commandToExecute.Run(command.Skip(1).ToArray());
+            
+            if (TryRunExternalCommand(command)) continue;
+            
+            CommandNotFound(command);
         }
+    }
+
+    private static bool TryRunExternalCommand(string[] command) {
+        var paths = Environment.GetEnvironmentVariable("PATH")?.Split(Path.PathSeparator);
+
+        if (paths == null) return false;
+
+        foreach (var path in paths) {
+            if (!Directory.Exists(path)) continue;
+            
+            var files = Directory.GetFiles(path, $"{command[0]}");
+            
+            if (files.Length > 0) {
+                using Process p = new Process();
+                
+                ProcessStartInfo info = new ProcessStartInfo(command[0]);
+
+                info.Arguments = string.Join(" ", command.Skip(1));
+                info.RedirectStandardInput = true;
+                info.RedirectStandardOutput = true;
+                info.UseShellExecute = false;
+                p.StartInfo = info;
+                p.Start();
+
+                string output = p.StandardOutput.ReadToEnd();
+
+                Console.WriteLine(output);
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static void CommandNotFound(string[]? command) {
